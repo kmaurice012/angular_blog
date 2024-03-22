23ff3fb36bac1fd2b8141d6dc5ed956a23f4dae7
kmaurice012 <kmaurice012@gitlab.com>
2024-03-22 10:03:41 +0300

    Add Styles And commet component props

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-comment',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './comment.component.css'
 })
 export class CommentComponent {
-
+  @Input() author!: string;
+  @Input() text!: string;
 }
