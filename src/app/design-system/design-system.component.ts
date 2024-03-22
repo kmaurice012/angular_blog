a1c5e767912b4c2ada80f02870ede2f71dc1e252
kmaurice012 <kmaurice012@gitlab.com>
2024-03-22 09:51:54 +0300

    Create Design System Component

---
+++ src/app/design-system/design-system.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-design-system',
+  standalone: true,
+  imports: [],
+  templateUrl: './design-system.component.html',
+  styleUrl: './design-system.component.css'
+})
+export class DesignSystemComponent {
+
+}
