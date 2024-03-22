071dff3b1d68800d12d6225d898c3cf2cdb553d3
kmaurice012 <kmaurice012@gitlab.com>
2024-03-22 10:01:44 +0300

    Create title,author and content props

--- src/app/blog-post/blog-post.component.ts
+++ src/app/blog-post/blog-post.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DesignSystemComponent } from '../design-system/design-system.component';
 
 @Component({
   selector: 'app-blog-post',
   standalone: true,
-  imports: [],
+  imports: [DesignSystemComponent],
   templateUrl: './blog-post.component.html',
-  styleUrl: './blog-post.component.css'
+  styleUrl: './blog-post.component.css',
 })
 export class BlogPostComponent {
-
+  @Input() title!: string;
+  @Input() author!: string;
+  @Input() content!: string;
 }
